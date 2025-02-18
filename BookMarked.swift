c04276aea92014b2d94c83fa9f40f767a65f8cce
WajanAltalhan <wajanaltalhan@gmail.com>
2025-02-18 23:02:13 +0300

    bookmarked screen done

--- BookMarked.swift
+++ BookMarked.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct BookMarked: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(.bg)
+                .ignoresSafeArea()
+            Image("aseer")
+                .offset(x: 60, y: 315)
+            VStack{
+                Text("Booked Marked \n Places !")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                    .offset(x: -50, y: -200)
+                VStack{
+                    ZStack{
+                        Rectangle()
+                            .fill(Color("rec"))
+                            .frame(width: 352, height: 126.5)
+                            .cornerRadius(20)
+                        Image("hc")
+                            .resizable()
+                            .frame(width:139, height:78)
+                            .offset(x: -90)
+                        HStack{
+                            VStack{
+                                Text("High City")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .multilineTextAlignment(.center)
+                                    .offset(x: 55, y: -10)
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                }
+                                .foregroundColor(Color("yelloww"))
+                                .offset(x: 66, y: 0)
+                                Text("Read Details")
+                                    .font(.subheadline)
+                                    .fontWeight(.medium)
+                                    .multilineTextAlignment(.leading)
+                                    .foregroundColor(.bluee)
+                                    .offset(x: 55, y: 10)
+                            }
+                            Image(systemName:"bookmark.fill")
+                                .foregroundColor(Color("greenn"))
+                                .font(.system(size: 30))
+                                .offset(x: 80, y: -20)
+                            
+                            
+                            
+                        }
+                    }
+                }
+                .offset(y: -180)
+            }
+        }
     }
 }
 
